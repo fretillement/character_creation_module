ac09f2fc5eebf74ff22d3c3ea9a0e16607054a47
Екатерина Худова <fretillement@yandex.ru>
2022-11-09 00:24:03 +0300

    my first commit

--- test_module.py
+++ test_module.py
@@ -1,13 +1,38 @@
-import os
+# Тестовые данные.
+TEST_DATA = [
+    (44, 'success', True),
+    (16, 'failure', True),
+    (4, 'success', False),
+    (21, 'failure', False),
+]
+
+BONUS = 1.1
+ANTIBONUS = 0.8
+
+def add_rep(current_rep, rep_points, buf_effect):
+    current_rep += rep_points
+    if buf_effect:
+        return current_rep * BONUS
+    return current_rep
+
+def remove_rep(current_rep, rep_points, debuf_effect):
+    current_rep -= rep_points
+    if debuf_effect:
+        return current_rep * ANTIBONUS
+    return current_rep
+
+def main(duel_res):
+    current_rep = 0.0
+    for rep, result, effect in duel_res:
+        if result == 'success':
+            current_rep = add_rep(current_rep, rep, effect)
+        if result == 'failure':
+            current_rep = remove_rep(current_rep, rep, effect)
+    return f'После {len(duel_res)} поединков, репутация персонажа — {current_rep:.3f} очков.'
 
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-dir_files = [filename.lower() for filename in os.listdir(BASE_DIR)]
-
-files_list = ['main.py', 'readme.md']
+# Тестовый вызов функции main.
+print(main(TEST_DATA))
 
 
-def test_program():
-    for filename in files_list:
-        assert filename in dir_files, f'Файл `{filename}` не найден в корне репозитория'
 
